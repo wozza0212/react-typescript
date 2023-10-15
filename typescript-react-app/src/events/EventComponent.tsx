8d795c826f88fa3eab5d259348cd023894d38146
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-10-15 20:28:16 +0100

    refactor: declare events earlier

--- typescript-react-app/src/events/EventComponent.tsx
+++ typescript-react-app/src/events/EventComponent.tsx
@@ -1,8 +1,21 @@
 const EventComponent: React.FC = () => {
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value)
+    }
+
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log('I am being dragged', event)
+    }
     return (
         <div>
             <h3>Event Component</h3>
-            <input onChange={(e) => console.log(e)} />
+            {/* <input onChange={(e) => console.log(e)} /> */}
+            <div>
+                <input onChange={onChange} />
+            </div>
+            <div draggable onDragStart={onDragStart}>
+                <h4>Drag Me</h4>
+            </div>
         </div>
     )
 }
