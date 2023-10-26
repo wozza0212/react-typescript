6742d56763aeff42308a4d723d901d463f92e87d
Thomas Wasnidge <thomas.wasnidge@sainsburys.co.uk>
2023-10-26 15:25:54 +0100

    removinbg any type

--- react-redux-project/redux-ts/src/state/reducers/repositoriesReducer.ts
+++ react-redux-project/redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,26 +1,43 @@
 interface RepositoriesState {
-    loading: boolean;
-    error: string | null;
-    data: string[];
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
-interface Action {
-    type: string;
-    payload?: any;
+interface SearchRepositoriesAction {
+  type: "search_repositories";
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
-    switch (action.type) {
-        case 'search_repositories':
-            return {loading: true, error: null, data: []};
-        case 'search_repositories_success':
-            return {loading: false, error: null, data: action.payload};
-        case 'search_repositories_error':
-            return {loading: false, error: action.payload, data: []}
-        default: 
-        return state
-    }
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
 
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
+}
+
+
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoriesState => {
+
+
+  switch (action.type) {
+    case "search_repositories":
+      return { loading: true, error: null, data: [] };
+    case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repositories_error":
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
 };
 
-export default reducer
+export default reducer;
