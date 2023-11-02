66156c277f099f7192dc8a10331daf4f7ca2f2f2
Thomas Wasnidge <thomas.wasnidge@sainsburys.co.uk>
2023-11-02 16:11:12 +0000

    project reload

---
+++ react-redux-project/redux-ts/src/state/actions/index.ts
@@ -0,0 +1,22 @@
+import { ActionType } from '../action-types'
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITOIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITOIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITOIES_ERROR;
+  payload: string;
+}
+
+
+export type Action =
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction;
+
